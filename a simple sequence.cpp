ff37f9644e5e85501aa7cec62090d093b016eb61
zahidjim <144053972+zahidjim@users.noreply.github.com>
2026-03-29 22:57:38 +0600

    Add files via upload

---
+++ a simple sequence.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define sortt(vec) sort(vec.begin(),vec.end())
+#define rsort(vec) sort(vec.rbegin(),vec.rend())
+#define take(vec) do{for(auto &it: vec) cin >> it;} while(0)
+#define show(vec) do{for(auto &it: vec) cout << it << ' '; cout << endl;} while(0)
+#define shaw(mape) do { for(auto const& [a, b] : (mape)) cout << a << " = " << b << endl; } while(0)
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    while(n)
+    {
+        cout << n-- << ' ';
+    }
+    cout << endl;
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
