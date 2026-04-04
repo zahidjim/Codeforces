115ff3a74b65f6f10f978856b5777cb23426bf06
zahidjim <144053972+zahidjim@users.noreply.github.com>
2026-04-04 22:24:25 +0600

    Add files via upload

---
+++ the 67th oeis problem.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a)*((b)/gcd(a,b)))
+#define sortt(vec) sort(vec.begin(),vec.end())
+#define rsort(vec) sort(vec.rbegin(),vec.rend())
+#define take(vec) do{for(auto &it: vec) cin >> it;} while(0)
+#define show(vec) do{for(auto &it: vec) cout << it << ' '; cout << endl;} while(0)
+#define shaw(mape) do { for(auto const& [a, b] : (mape)) cout << a << " = " << b << endl; } while(0)
+using namespace std;
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << 1 << ' ';
+    for(ll i=1;i<n;++i)
+    {
+        cout << (2*i-1)*(2*i+1) << ' ';
+    }
+    cout << endl;
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ the 67th permutation problem.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a)*((b)/gcd(a,b)))
+#define sortt(vec) sort(vec.begin(),vec.end())
+#define rsort(vec) sort(vec.rbegin(),vec.rend())
+#define take(vec) do{for(auto &it: vec) cin >> it;} while(0)
+#define show(vec) do{for(auto &it: vec) cout << it << ' '; cout << endl;} while(0)
+#define shaw(mape) do { for(auto const& [a, b] : (mape)) cout << a << " = " << b << endl; } while(0)
+using namespace std;
+void solve()
+{
+    int n,e;
+    cin >> n;
+    e = 3*n-1;
+
+    for(int i=1;i<=n;++i)
+    {
+        cout << i << ' ' << e << ' ' << e+1 << ' ';
+        e -= 2;
+    }
+    cout << endl;
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
