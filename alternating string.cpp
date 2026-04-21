9d4ecb51ba72f40e75a1ee5356ce2ca323efdb0d
zahidjim <144053972+zahidjim@users.noreply.github.com>
2026-04-21 21:49:33 +0600

    Add files via upload

---
+++ alternating string.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a)*((b)/gcd(a,b)))
+#define sortt(vec) sort(vec.begin(),vec.end())
+#define rsort(vec) sort(vec.rbegin(),vec.rend())
+#define take(vec) do{for(auto &it: vec) cin >> it;} while(0)
+#define show(vec) do{for(auto &it: vec) cout << it << ' '; cout << endl;} while(0)
+#define shaw(mape) do { for(auto const& [a, b] : (mape)) cout << a << " = " << b << endl; } while(0)
+using namespace std;
+bool solve()
+{
+    int cnt=0,n;
+    string str;
+    cin >> str;
+    n = str.size();
+
+    for(int i=1;i<n;++i)
+    {
+        if(str[i]==str[i-1])
+        {
+            ++cnt;
+        }
+    }
+    return (cnt<3);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        cout << (solve()?"YES":"NO") << endl;
+    }
+    return 0;
+}
+
