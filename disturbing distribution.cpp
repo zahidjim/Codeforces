d7c424b4178ab7d90853b2222b69d2023c1ee83d
zahidjim <144053972+zahidjim@users.noreply.github.com>
2026-04-29 21:55:45 +0600

    Add files via upload

---
+++ disturbing distribution.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a)*((b)/gcd(a,b)))
+#define sortt(vec) sort(vec.begin(),vec.end())
+#define rsort(vec) sort(vec.rbegin(),vec.rend())
+#define take(vec) do{for(auto &it: vec) cin >> it;} while(0)
+#define show(vec) do{for(auto &it: vec) cout << it << ' '; cout << endl;} while(0)
+#define shaw(mape) do { for(auto const& [a, b] : (mape)) cout << a << " = " << b << endl; } while(0)
+using namespace std;
+ll solve()
+{
+    ll n,sum=0;
+    cin >> n;
+    vector<ll>vec(n);
+
+    take(vec);
+
+    for(ll i=0;i<n;++i)
+    {
+        if(vec[i]!=1)
+        {
+            sum += vec[i];
+        }
+    }
+
+    if(vec.back()==1)
+    {
+        ++sum;
+    }
+    return (sum%676767677);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        cout << solve() << endl;
+    }
+    return 0;
+}
---
+++ everything everywhere.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) ((a)*((b)/gcd(a,b)))
+#define sortt(vec) sort(vec.begin(),vec.end())
+#define rsort(vec) sort(vec.rbegin(),vec.rend())
+#define take(vec) do{for(auto &it: vec) cin >> it;} while(0)
+#define show(vec) do{for(auto &it: vec) cout << it << ' '; cout << endl;} while(0)
+#define shaw(mape) do { for(auto const& [a, b] : (mape)) cout << a << " = " << b << endl; } while(0)
+using namespace std;
+ll solve()
+{
+    ll n,ans = 0;
+    cin >> n;
+    vector<ll>vec(n);
+    take(vec);
+
+    for(ll i=1;i<n;++i)
+    {
+        if(abs(vec[i]-vec[i-1])==gcd(vec[i],vec[i-1]))
+        {
+            ++ans;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        cout << solve() << endl;
+    }
+    return 0;
+}
